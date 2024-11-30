e25b18870a8eb47d6002ec7e5063a38c2aea011b
Scolastica Nzyoki <solanzioki18@gmail.com>
2024-11-30 22:39:06 +0300

    grade

---
+++ Grade.js
@@ -0,0 +1,21 @@
+function studentMarks(grade) {
+    if (grade > 79) {
+        return "A";
+    }
+    else if (grade >= 60 && grade <= 79) {
+        return "B";
+    }
+    else if (grade >= 49 && grade <= 59) {
+        return "C";
+    }
+    else if (grade >= 40 && grade <= 48) {
+        return "D";
+    }
+    else {
+        return "E";
+    }
+
+}
+
+const score = prompt("Enter Marks: ");
+console.log(studentMarks(score));
